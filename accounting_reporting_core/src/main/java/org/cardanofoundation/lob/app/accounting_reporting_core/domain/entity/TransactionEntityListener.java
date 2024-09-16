2187349263cccdae4239f66b0f57ecae1b13889b
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2024-09-16 15:55:38 +0200

    feat: final reconcilation status as well as removal of sink diff.

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/entity/TransactionEntityListener.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/entity/TransactionEntityListener.java
@@ -2,13 +2,28 @@ package org.cardanofoundation.lob.app.accounting_reporting_core.domain.entity;
 
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.ReconcilationCode;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionStatus;
+import org.cardanofoundation.lob.app.support.collections.Optionals;
+
+import java.util.Optional;
 
 import static org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionStatus.NOK;
 import static org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.TransactionStatus.OK;
 import static org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.ValidationStatus.VALIDATED;
 
 public class TransactionEntityListener {
+
+    @PrePersist
+    @PreUpdate
+    public void update(TransactionEntity transactionEntity) {
+        transactionEntity.setOverallStatus(calcStatus(transactionEntity));
+
+        transactionEntity.setReconcilation(transactionEntity.getReconcilation().map(r -> r.toBuilder()
+                .finalStatus(reconcilationCode(transactionEntity).orElse(null))
+                .build()
+        ));
+    }
 
     protected TransactionStatus calcStatus(TransactionEntity transactionEntity) {
         if (transactionEntity.allApprovalsPassedForTransactionDispatch()
@@ -21,10 +36,16 @@ public class TransactionEntityListener {
         return NOK;
     }
 
-    @PrePersist
-    @PreUpdate
-    public void update(TransactionEntity transactionEntity) {
-        transactionEntity.setOverallStatus(calcStatus(transactionEntity));
+    protected Optional<ReconcilationCode> reconcilationCode(TransactionEntity transactionEntity) {
+        return transactionEntity.getReconcilation().flatMap(r -> {
+            return Optionals.zip(r.getSource(), r.getSink(), (source, sink) -> {
+                if (source == ReconcilationCode.OK && sink == ReconcilationCode.OK) {
+                    return ReconcilationCode.OK;
+                }
+
+                return ReconcilationCode.NOK;
+            });
+        });
     }
 
 }
