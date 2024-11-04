85768c19f22ea2b537c75b27446690f58dde70e8
Marco Russo <marco.russo@cardanofoundation.org>
2024-11-04 22:58:53 +0000

    fix :reconciled_result_count

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/requests/ReconciliationRejectionCodeRequest.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/requests/ReconciliationRejectionCodeRequest.java
@@ -10,9 +10,10 @@ public enum ReconciliationRejectionCodeRequest {
     NEW_VERSION_NOT_PUBLISHED,
     NEW_VERSION;
 
-    public static ReconciliationRejectionCodeRequest of(ReconcilationRejectionCode code) {
+    public static ReconciliationRejectionCodeRequest of(ReconcilationRejectionCode code, Boolean ledgerDispatchApproval) {
         return switch (code) {
-            case SOURCE_RECONCILATION_FAIL -> ReconciliationRejectionCodeRequest.NEW_VERSION;
+            case SOURCE_RECONCILATION_FAIL ->
+                    ledgerDispatchApproval ? ReconciliationRejectionCodeRequest.NEW_VERSION : ReconciliationRejectionCodeRequest.NEW_VERSION_NOT_PUBLISHED;
             case SINK_RECONCILATION_FAIL -> ReconciliationRejectionCodeRequest.IN_PROCESSING;
             case TX_NOT_IN_ERP -> ReconciliationRejectionCodeRequest.MISSING_IN_ERP;
             case TX_NOT_IN_LOB -> ReconciliationRejectionCodeRequest.NEW_IN_ERP;
