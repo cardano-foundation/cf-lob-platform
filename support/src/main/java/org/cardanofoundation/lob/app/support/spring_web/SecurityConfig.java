8b6e554f464c0bbc4fe3a0bbdccff7a250585f86
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-12-16 14:29:18 +0100

    chore: updates

--- support/src/main/java/org/cardanofoundation/lob/app/support/spring_web/SecurityConfig.java
+++ support/src/main/java/org/cardanofoundation/lob/app/support/spring_web/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll() // allowing all access
                 )
