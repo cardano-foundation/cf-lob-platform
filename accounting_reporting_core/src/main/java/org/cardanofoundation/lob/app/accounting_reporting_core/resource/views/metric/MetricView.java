a113e78d89fcadd1e935258f04f46e151763a6e7
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-01-15 08:16:33 +0100

    chore: moved files

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/views/metric/MetricView.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/views/metric/MetricView.java
@@ -1,4 +1,4 @@
-package org.cardanofoundation.lob.app.accounting_reporting_core.resource.views;
+package org.cardanofoundation.lob.app.accounting_reporting_core.resource.views.metric;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.cardanofoundation.lob.app.accounting_reporting_core.domain.core.metric.MetricEnum;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
