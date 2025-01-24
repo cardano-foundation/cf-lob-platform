62896728686393a8a086e4a261304ef6e3f48e5d
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-01-24 06:48:25 +0100

    chore: fixed build warning of mapper

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/mapper/ChartViewMapper.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/mapper/ChartViewMapper.java
@@ -5,11 +5,17 @@ import org.cardanofoundation.lob.app.accounting_reporting_core.resource.views.me
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import javax.annotation.MatchesPattern;
+
 @Mapper(componentModel = "spring")
 public interface ChartViewMapper {
 
     @Mapping(target = "xPos", source = "chartView.XPos")
     @Mapping(target = "yPos", source = "chartView.YPos")
+    // ID is set by the database
+    @Mapping(target = "id", ignore = true)
+    // Dashboard is set by the DashboardViewMapper
+    @Mapping(target = "dashboard", ignore = true)
     ChartEntity toChartEntity(ChartView chartView);
 
 }
