ac6a8d9c9c963bf121285933c8795087f94f4a7f
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-01-28 06:35:13 +0100

    chore: spotless fixes

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/service/internal/ERPIncomingDataProcessor.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/service/internal/ERPIncomingDataProcessor.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
