af404df86b26521466cebaec461b803964cf2a0f
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-02-21 11:25:47 +0100

    fix: added check to avoid pkey violation (#152)
    * fix: added check to avoid pkey violation
    
    * chore: adding tests
    
    * chore: testing
    
    * fix: fixed a bug which hindered the creation of TransactionBatchFailedEvent
    
    * chore: removed service since the bean will be created in the using app
    
    * chore: spotless
    
    * chore: removed unused code

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/event/extraction/TransactionBatchFailedEvent.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/event/extraction/TransactionBatchFailedEvent.java
@@ -23,6 +23,7 @@ import org.cardanofoundation.lob.app.support.modulith.EventMetadata;
 @ToString
 @NoArgsConstructor
 @Externalized
+@Setter
 public class TransactionBatchFailedEvent {
 
     public static final String VERSION = "1.0";
