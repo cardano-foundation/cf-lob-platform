278612a896022eb2075c1e46c2ed99dd6dfc2ada
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-02-24 10:59:04 +0100

    fix: kafka couldn't deserialize blockchain receipt (#160)

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/core/BlockchainReceipt.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/domain/core/BlockchainReceipt.java
@@ -2,9 +2,13 @@ package org.cardanofoundation.lob.app.accounting_reporting_core.domain.core;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class BlockchainReceipt {
 
     private String type;
