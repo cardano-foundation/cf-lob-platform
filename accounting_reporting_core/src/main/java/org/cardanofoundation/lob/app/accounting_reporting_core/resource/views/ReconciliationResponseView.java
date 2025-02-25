0c4303695288c441f0daf76ce597bf5be1b1df81
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2025-02-25 07:42:38 +0100

    feat: add data about last reconcilation + refactored

--- accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/views/ReconciliationResponseView.java
+++ accounting_reporting_core/src/main/java/org/cardanofoundation/lob/app/accounting_reporting_core/resource/views/ReconciliationResponseView.java
@@ -1,12 +1,15 @@
 package org.cardanofoundation.lob.app.accounting_reporting_core.resource.views;
 
+import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
@@ -14,6 +17,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @AllArgsConstructor
 public class ReconciliationResponseView {
     private Long total = 0L;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Optional<LocalDate> lastDateFrom;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Optional<LocalDate> lastDateTo;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Optional<LocalDate> lastReconciledDate;
     private TransactionReconciliationStatisticView statistic;
 
     @JsonProperty("transactions")
