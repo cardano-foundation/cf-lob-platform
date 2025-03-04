cf3941f2418ce4678493e62721691404c7a9540a
Marco Russo <marco.russo@cardanofoundation.org>
2025-03-04 10:15:54 +0000

    fix: change organisation update endpoint

--- organisation/src/main/java/org/cardanofoundation/lob/app/organisation/domain/request/OrganisationUpdate.java
+++ organisation/src/main/java/org/cardanofoundation/lob/app/organisation/domain/request/OrganisationUpdate.java
@@ -1,5 +1,7 @@
 package org.cardanofoundation.lob.app.organisation.domain.request;
 
+import javax.annotation.Nullable;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,33 +15,43 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @NoArgsConstructor
 public class OrganisationUpdate {
 
+    @Nullable
     @Schema(example = "My company name")
     private String name;
 
+    @Nullable
     @Schema(example = "Ballyhealy")
     private String city;
 
+    @Nullable
     @Schema(example = "Y35 C6KC")
     private String postCode;
 
+    @Nullable
     @Schema(example = "Co. Wexford")
     private String province;
 
+    @Nullable
     @Schema(example = "Ballyhealy Cottage")
     private String address;
 
+    @Nullable
     @Schema(example = "0035863286566")
     private String phoneNumber;
 
+    @Nullable
     @Schema(example = "lob@cardanofoundation.org")
     private String adminEmail;
 
+    @Nullable
     @Schema(example = "http://cardanofoundation.org")
     private String websiteUrl;
 
+    @Nullable
     @Schema(example = "ISO_4217:CHF")
     private String currencyId;
 
+    @Nullable
     @Schema(example = "ISO_4217:CHF")
     private String reportCurrencyId;
 
