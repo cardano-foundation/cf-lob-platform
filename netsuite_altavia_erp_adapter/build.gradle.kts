fade59679abfb616f5a30a4dbbfa93870a277276
Thomas Kammerlocher <thomas.kammerlocher@cardanofoundation.org>
2025-03-10 09:30:44 +0100

    feat: added oAuth2.0 support (#178)
    * feat: added oAuth2.0 support
    
    * feat: added oAuth2.0 support
    
    * chore: moved response files
    
    * chore: spotless
    
    * chore: replaced rest library, refactored & fixed a bug in transaction converter
    
    * feat: added support for mock mode
    
    * feat: added support for mock mode
    
    * chore: added qualifier name to blockchainreaderRestClientConfig

--- netsuite_altavia_erp_adapter/build.gradle.kts
+++ netsuite_altavia_erp_adapter/build.gradle.kts
@@ -1,4 +1,8 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     implementation(project(":accounting_reporting_core"))
     implementation(project(":organisation"))
